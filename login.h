64a468e5ddc682d1cc781d1c9180e4dd8381a6d9
kareemt <kareemtamer05@aucegypt.edu>
2024-05-03 02:35:41 +0300

    skeleton for login

---
+++ login.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<map>
+#include<string>
+class login_class
+{
+    private:
+    std::map<std::string,std::string> login_record;
+    public:
+    bool check()
+    {
+
+    }
+    void registering ()
+    {
+
+    }
+   void login()
+    {
+
+    }
+};
